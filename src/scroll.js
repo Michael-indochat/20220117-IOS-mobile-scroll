64c9c114323e73c6a3ca240bb3f47f0041509f4b
Michael Luo <michael@indochat.co.id>
2023-01-17 12:07:27 +0800

    update

--- src/scroll.js
+++ src/scroll.js
@@ -1,64 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("page init");
   // Select all elements with tag name 'html' and 'body'
-  const docEl = document.querySelectorAll("html, body"),
-    // Select the first element with class name 'content'
-    wrap = document.querySelector(".content"),
-    // Select the first button
-    button = document.querySelector("button"),
-    // Select the first element with class name 'close'
-    close = document.querySelector(".close"),
-    // Get the current scroll position
-    scrollTop = window.pageYOffset;
+  let button = document.querySelectorAll("button");
+  let docEl = document.querySelectorAll("html, body");
+  // Select the first element with class name 'content'
+  let wrap = document.getElementById("app");
+  // Select the first element with class name 'close'
+  let close = document.querySelector(".close");
+  // Get the current scroll position
+  let scrollTop = window.pageYOffset;
 
   // Add click event listener to button
-  button.addEventListener("click", (e) => {
-    overlayOpen();
-    e.preventDefault();
+  button.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      dialogOpen();
+      e.preventDefault();
+    });
   });
 
   // Add click event listener to close button
   close.addEventListener("click", (e) => {
-    overlayClose();
+    dialogClose();
     e.preventDefault();
   });
 
-  // Function to close overlay
-  const overlayClose = () => {
+  // Function to close dialog
+  const dialogClose = () => {
     unlockBody();
-    document.body.classList.remove("overlay-open");
+    document.body.classList.remove("dialog-open"); // remove block
   };
-  // Function to open overlay
-  const overlayOpen = () => {
-    document.body.classList.add("overlay-open");
+  // Function to open dialog
+  const dialogOpen = () => {
+    document.body.classList.add("dialog-open"); // block
     lockBody();
   };
 
   // Function to lock the body
   const lockBody = () => {
+    console.log("lock");
     if (scrollTop) {
-      wrap.style.top = -scrollTop + "px";
+      console.log("scrollTop", scrollTop);
+      wrap.style.top = `-${scrollTop}px`;
     }
 
     // Change the height and overflow style of all elements in the docEl array
     docEl.forEach((el) => {
-      el.style.height = "100%";
       el.style.overflow = "hidden";
     });
   };
 
   // Function to unlock the body
   const unlockBody = () => {
+    console.log("unlock");
     // Change the height and overflow style of all elements in the docEl array
     docEl.forEach((el) => {
-      el.style.height = "";
       el.style.overflow = "";
     });
 
     wrap.style.top = "";
-
-    window.scrollTo(0, scrollTop);
-    setTimeout(() => {
-      scrollTop = null;
-    }, 0);
   };
 });
